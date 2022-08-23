4dff1795054a2f4c18c43a12300523133bea2326
Chen Zou <chenzou@uchicago.edu>
2022-08-23 04:11:18 +0000

    tests: to merge
    Change-Id: I438dc83fc29891ae08223f53da62060842f22ce2

--- tests/test-progs/armsve/build.sh
+++ tests/test-progs/armsve/build.sh
@@ -1,6 +1,9 @@
 ~/llvm-project/buildeddebug/bin/clang -fintegrated-cc1 -v -emit-llvm -std=c++17 -static --gcc-toolchain=$HOME/arm --sysroot=$HOME/arm/aarch64-none-linux-gnu/libc --target=aarch64-linux-gnueabi -march=armv8.2-a+sve -Wall -Wextra -O3 -o sve.ll -S ./sve.cc
 ~/llvm-project/buildeddebug/bin/llc -debug -march=aarch64 -mattr=+v8.2a,+sve ./sve.ll -o sve.s 2> llc.debug
 
+# ~/llvm-project/buildeddebug/bin/clang -fintegrated-cc1 -v -emit-llvm -std=c++17 -static --gcc-toolchain=$HOME/arm --sysroot=$HOME/arm/aarch64-none-linux-gnu/libc --target=aarch64-linux-gnueabi -march=armv8.2-a+sve -Wall -Wextra -O0 -o sve.ll -S ./sve.cc
+# ~/llvm-project/buildeddebug/bin/llc -debug -O0 -march=aarch64 -mattr=+v8.2a,+sve ./sve.ll -o sve.s 2> llc.debug
+
 # ~/llvm-project/buildeddebug/bin/clang -v -std=c++17 -static --gcc-toolchain=$HOME/arm --sysroot=$HOME/arm/aarch64-none-linux-gnu/libc --target=aarch64-linux-gnueabi -march=armv8.2-a+sve -Wall -Wextra -O2 -o sve.static ./sve.cc -v && \
 #     ~/llvm-project/buildeddebug/bin/llvm-objdump --arch-name=aarch64 --mattr=+v8.2a,+sve -S ./sve.static > sve.objdump
 
--- tests/test-progs/armsve/sve.cc
+++ tests/test-progs/armsve/sve.cc
@@ -1,52 +1,56 @@
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 
 #include "arm_neon.h"
 #include "arm_sve.h"
 
 int main() {
   const int N = 96;
-  uint8_t a[N + N] = {71,  62, 111, 245, 33,  128, 32, 64,  71,  62, 111,
-                      245, 33, 128, 32,  64,  71,  62, 111, 245, 33, 128,
-                      32,  64, 71,  62,  111, 245, 33, 128, 32,  64,
-                      71,  62, 111, 245, 33,  128, 32, 64,  71,  62, 111,
-                      245, 33, 128, 32,  64,  71,  62, 111, 245, 33, 128,
-                      32,  64, 71,  62,  111, 245, 33, 128, 32,  64,
-                      71,  62, 111, 245, 33,  128, 32, 64,  71,  62, 111,
-                      245, 33, 128, 32,  64,  71,  62, 111, 245, 33, 128,
-                      32,  64, 71,  62,  111, 245, 33, 128, 32,  64};
-  uint8_t b[N + N] = {0, 0, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7,
-    6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6,
-    7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7,
-    8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8,
-    6, 8, 6, 7};
+  uint8_t a[N + N] = {
+      71, 62, 111, 245, 33, 128, 32, 64, 71, 62, 111, 245, 33, 128, 32, 64,
+      71, 62, 111, 245, 33, 128, 32, 64, 71, 62, 111, 245, 33, 128, 32, 64,
+      71, 62, 111, 245, 33, 128, 32, 64, 71, 62, 111, 245, 33, 128, 32, 64,
+      71, 62, 111, 245, 33, 128, 32, 64, 71, 62, 111, 245, 33, 128, 32, 64,
+      71, 62, 111, 245, 33, 128, 32, 64, 71, 62, 111, 245, 33, 128, 32, 64,
+      71, 62, 111, 245, 33, 128, 32, 64, 71, 62, 111, 245, 33, 128, 32, 64};
+  uint8_t b[N + N] = {0, 0, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6,
+                      7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7,
+                      7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7,
+                      6, 7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6,
+                      7, 8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7, 7, 6, 7,
+                      8, 6, 8, 6, 7, 7, 6, 7, 8, 6, 8, 6, 7};
 
-  svint32_t v = svvload((int8_t*) a, (int8_t*)b);
-  svbool_t at = svptrue_b8();
+  svint32_t v = svvload((int8_t *)a, (int8_t *)b);
 
-  v = svmax_n_s32_m(at, v, 124);
-  v = svvmax_n_m(at, v, 127);
-  v = svvadd_n_m(at, v, 126);
-  v = svvadd_n_m(at, v, 1024);
-  v = svvsub_n_m(at, v, 126);
-  v = svvsub_n_m(at, v, 512);
-  v = svvmul_n_m(at, v, 126);
-  v = svvasl_m(at, v, 2);
-  svbool_t pg = svvcmple(at, v, svvcpz(at, 16));
+  v = svmax_n_s32_m(svptrue_b8(), v, 124);
+  v = svvmax_n_m(svptrue_b8(), v, 127);
+  v = svvadd_n_m(svptrue_b8(), v, 126);
+  v = svvadd_n_m(svptrue_b8(), v, 1024);
+  v = svvsub_n_m(svptrue_b8(), v, 126);
+  v = svvsub_n_m(svptrue_b8(), v, 512);
+  v = svvmul_n_m(svptrue_b8(), v, 126);
+  v = svvasl_m(svptrue_b8(), v, 2);
+  svbool_t pg = svvcmple(svptrue_b8(), v, svvcpz(svptrue_b8(), 16));
 
-  svvstore(v, (int8_t*)a+100, (int8_t*)200);
+  svvstore(v, (int8_t *)a + 100, (int8_t *)200);
 
-  v = svvcpz(at, 128 * 6);
+  v = svvcpz(svptrue_b8(), 128 * 6);
   v = svvcpy_m(v, pg, 128 * 16);
 
-  v = svvand_n_m(at, v, 511);
-  v = svvand_n_m(at, v, 63);
-  v = svvbic_n_m(at, v, 63);
-  v = svvpsum(at, v);
-  v = svvabs_m(v, at, v);
+  v = svvand_n_m(svptrue_b8(), v, 511L);
+  v = svvand_n_m(svptrue_b8(), v, 63);
+  v = svvand_n_m(svptrue_b8(), v, 0xf1f1f1f1f1f1f1f1L);
+  v = svvand_n_m(svptrue_b8(), v, 0x00000000003fff80L);
+  v = svvbic_n_m(svptrue_b8(), v, 511);
+  v = svvbic_n_m(svptrue_b8(), v, 63);
+  v = svvbic_n_m(svptrue_b8(), v, 0xf1f1f1f1f1f1f1f1L);
+  v = svvbic_n_m(svptrue_b8(), v, 0x00000000003fff80L);
+  v = svvpsum(svptrue_b8(), v);
+  v = svvabs_m(v, svptrue_b8(), v);
 
-  int ans = svvaddv(at, v);
-  ans += svvnum(at, v);
+  int ans = svvaddv(svptrue_b8(), v);
+  ans += svvnum(svptrue_b8(), v);
   ans += svvrnum();
   ans += svvrcnum();
 
